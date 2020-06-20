4d605316f7918f786bc325c1b650f9f7cd2069da
Steve Brown <42909235+shoddyguard@users.noreply.github.com>
2020-06-20 09:34:59 +0100

    Create Invoke-SlackNotification.ps1
    Add the Invoke-SlackNotification cmdlet

---
+++ PowerShellModule/Public/Invoke-SlackNotification.ps1
@@ -0,0 +1,85 @@
+<#
+.SYNOPSIS
+    Simple cmdlet for sending a Slack message.
+.DESCRIPTION
+    This cmdlet allows you to send a basic Slack by simply invoking it with the '-message' and '-webhook' parameters.
+    It's intended to be used directly in other modules/scripts for a cleaner way of invoking a Slack message.
+    You can optionally declare the '-colour' (sorry for the spelling but I'm British) and the '-push' parameters to set a notification colour and a toast notification message.
+.EXAMPLE
+    PS C:\> Invoke-SlackNotification -Message 'Hello, world!' -Webhook 'https://hooks.slack.com/my-webhook'
+    This would send the 'Hello, world!' message to the channel linked to the given webhook
+.EXAMPLE
+    PS C:\> Invoke-SlackNotification -Message 'Foo Bar' -Webhook 'https://hooks.slack.com/my-webhook' -Colour '#FF0000' -Push 'Fizz Buzz'
+    This would send the 'Foo Bar' message to the channel linked to the given webhook. 
+    The message would also have a red stripe down the side of it and the pop-up notification would say 'Fizz Buzz'
+.NOTES
+    More info on the colour and push (fallback) options can be found at: https://api.slack.com/docs/message-attachments
+#>
+function Invoke-SlackNotification
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(
+            Mandatory = $true,
+            HelpMessage = "The message you would like sent to Slack"
+        )]
+        [string]$message,
+
+        [Parameter(
+            Mandatory = $true,
+            HelpMessage = "The Webhook for the channel/DM you'd like the message to be sent"
+        )] 
+        [string]$webhook, 
+    
+        [Parameter(
+            Mandatory = $false,
+            HelpMessage = "Hex code you would like on the side of the message (if any)"
+        )]
+        [string]$colour, 
+    
+        [Parameter(
+            Mandatory = $false,
+            HelpMessage = "For mobile devices, the content that appears in the notification bar/area"
+        )]
+        [string]$push
+    )
+    # Let's initialize an empty hash table
+    $slackbody = 
+    @{
+        attachments = 
+        @(
+            @{
+            }
+        )
+    }
+
+    # Add any given optional parameters to the hash table.
+    if ($colour)
+    {
+        ($slackbody.attachments)[0] += 
+        @{
+            'color' = $colour
+        }
+    }
+
+    if ($push)
+    {
+        ($slackbody.attachments)[0] += 
+        @{
+            'fallback' = $push
+        }
+    }
+
+    ($slackbody.attachments)[0] += 
+    @{
+        'text' = $message
+    }
+    try
+    {
+        invoke-restmethod -Uri $webhook -Method Post -Body (ConvertTo-Json $slackbody) -ErrorAction Stop
+    }
+    catch
+    {
+        throw "Failed to send Slack notification.$($_.Exception.Message)"
+    }
+}
